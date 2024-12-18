345eabb38926a34607125855d108d91de1c661bf
DrDy <vide.vinluan.up@phinmaed.com>
2024-12-18 10:36:36 +0800

    first commit

--- IntelliFare/MainPage.xaml.cs
+++ IntelliFare/MainPage.xaml.cs
@@ -1,10 +1,12 @@
-﻿namespace IntelliFare
+﻿using DotNetEnv;
+namespace IntelliFare
 {
     public partial class MainPage : ContentPage
     {
         public MainPage()
         {
             InitializeComponent();
+            DotNetEnv.Env.Load();
         }
     }
 }
