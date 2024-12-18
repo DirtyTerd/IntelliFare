59ef086d9f4d58bea6943367e06c82b42a1fdd35
DrDy <vide.vinluan.up@phinmaed.com>
2024-12-18 10:33:54 +0800

    Add project files.

---
+++ IntelliFare/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace IntelliFare
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new MainPage();
+        }
+    }
+}
---
+++ IntelliFare/MainPage.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace IntelliFare
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
